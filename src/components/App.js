471cb3b3dc236d6f55feadf288a7bb6da28d3a22
yakoveka <yakoveka@gmail.com>
2021-05-31 00:05:13 +0700

    init commit2

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './Dashboard.js';
+import Login from './Login.js';
+import Preferences from './Preferences.js';
+import useToken from './useToken';
+
+function setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token
+}
+
+function App() {
+
+    const { token, setToken } = useToken();
+
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preferences />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
